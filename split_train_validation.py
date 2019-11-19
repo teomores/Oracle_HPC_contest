36937dadac1e9e17209cab912afe806356239f4c
alessiorussointroito <alessio2.russo@mail.polimi.it>
2019-11-19 15:56:32 +0100

    splitting train in train-validation

---
+++ split_train_validation.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+train = pd.read_csv("dataset/original/train.csv", escapechar="\\")
+
+X_train, X_val, y_train, y_val = train_test_split(train, train['linked_id'] , test_size=0.33, random_state=42)
+X_train.to_csv('dataset/validation/X_train.csv', index=True)
+X_val.to_csv('dataset/validation/X_val.csv', index=True)
+#y_train.to_csv('dataset/validation/y_train.csv', index=True)
+#y_val.to_csv('dataset/validation/y_val.csv', index=True)
+
+
+# Reading dataset keeping the saved indices:
+# train = pd.read_csv('dataset/validation/X_train.csv', index_col=0)
