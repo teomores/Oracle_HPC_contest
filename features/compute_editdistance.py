0c5c8c2bbfe43ed1fe3e55ece0301f21089323d7
alessiorussointroito <alessio2.russo@mail.polimi.it>
2019-11-26 15:35:29 +0100

    XGB dataset generation

--- features/compute_editdistance.py
+++ features/compute_editdistance.py
@@ -3,7 +3,8 @@ import numpy as np
 from tqdm import tqdm
 import editdistance
 
-def editdistance(df_exp, validation=True):
+
+def compute_editdistance(df_exp, validation=True):
     if validation:
         df_test = pd.read_csv("../dataset/validation/test.csv", escapechar="\\")
         df_train = pd.read_csv("../dataset/validation/train.csv", escapechar="\\")
@@ -14,11 +15,16 @@ def editdistance(df_exp, validation=True):
     df_train = df_train.sort_values(by='record_id').reset_index(drop=True)
     df_test = df_test.sort_values(by='record_id').reset_index(drop=True)
 
-    df_exp = df_exp.merge(df_test[['record_id', 'name']], how='left', left_on='queried_record_id', right_on='record_id').drop('record_id', axis=1)
+    df_exp = df_exp.merge(df_test[['record_id', 'name']], how='left', left_on='queried_record_id',
+                          right_on='record_id').drop('record_id', axis=1)
     df_exp = df_exp.rename(columns={'name': 'queried_name'})
 
-    df_exp = df_exp.merge(df_train[['record_id', 'name']], how='left', left_on='linked_id_idx', right_index=True).drop('record_id', axis=1)
+    df_exp = df_exp.merge(df_train[['record_id', 'name']], how='left', left_on='linked_id_idx', right_index=True).drop(
+        'record_id', axis=1)
     df_exp = df_exp.rename(columns={'name': 'predicted_name'})
+
+    print(f'NaN on queried_name: {df_exp.queried_name.isna().sum()}')
+    print(f'Nan on predicted_name: {df_exp.predicted_name.isna().sum()}')
 
     df_exp['queried_name'] = df_exp.queried_name.fillna('')
     df_exp['predicted_name'] = df_exp.predicted_name.fillna('')
@@ -27,7 +33,7 @@ def editdistance(df_exp, validation=True):
     df_exp['predicted_name'] = df_exp.predicted_name.str.lower()
 
     def extract_editdistance(queried_name, predicted_name):
-        res = np.empty(len(queried_name))
+        res = np.empty(len(queried_name), dtype=int)
         for i in tqdm(range(len(queried_name))):
             try:
                 res[i] = editdistance.eval(queried_name[i], predicted_name[i])
@@ -36,6 +42,7 @@ def editdistance(df_exp, validation=True):
         return res
 
     df_exp['editdistance'] = extract_editdistance(df_exp.queried_name.values, df_exp.predicted_name.values)
-    df_exp['editdistance'] = df_exp.astype(int)
-    return df_exp
+    df_exp = df_exp.drop(['queried_name', 'predicted_name'], axis=1)
+    return df_exp['editdistance']
+
 
