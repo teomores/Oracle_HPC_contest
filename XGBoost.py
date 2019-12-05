79498b27cf592f299bd91aa2613fb212bd0801e8
alessiorussointroito <alessio2.russo@mail.polimi.it>
2019-12-05 19:02:30 +0100

    added scores and record to final dataframe

--- XGBoost.py
+++ XGBoost.py
@@ -26,7 +26,7 @@ ranker.fit(train.drop(['queried_record_id', 'target', 'linked_id_idx'], axis=1),
 
 predictions = ranker.predict(test.drop(['queried_record_id', 'linked_id_idx'], axis=1))
 test['predictions'] = predictions
-df_predictions = test[['queried_record_id', 'predicted_record_id', 'predictions']]
+df_predictions = test[['queried_record_id', 'predicted_record_id', 'predicted_record_id_record', 'predictions']]
 
 rec_pred = []
 for (r,p) in zip(df_predictions.predicted_record_id, df_predictions.predictions):
@@ -38,19 +38,25 @@ group_queried = df_predictions[['queried_record_id', 'rec_pred']].groupby('queri
 df_predictions = pd.DataFrame(group_queried).reset_index().rename(columns={0 : 'rec_pred'})
 
 def reorder_preds(preds):
-    sorted_list = []
+    ordered_lin = []
+    ordered_score = []
+    ordered_record = []
     for i in range(len(preds)):
         l = sorted(preds[i], key=lambda t: t[1], reverse=True)
-        l = [x[0] for x in l]
-        sorted_list.append(l)
-    return sorted_list
+        lin = [x[0] for x in l]
+        s = [x[1] for x in l]
+        r = [x[2] for x in l]
+        ordered_lin.append(lin)
+        ordered_score.append(s)
+        ordered_record.append(r)
+    return ordered_lin, ordered_score, ordered_record
 
-df_predictions['ordered_preds'] = reorder_preds(df_predictions.rec_pred.values)
-df_predictions = df_predictions[['queried_record_id', 'ordered_preds']].rename(columns={'ordered_preds': 'predicted_record_id'})
+df_predictions['ordered_linked'], df_predictions['ordered_scores'], df_predictions['ordered_record'] = reorder_preds(df_predictions.rec_pred.values)
+#df_predictions = df_predictions[['queried_record_id', 'ordered_preds']].rename(columns={'ordered_preds': 'predicted_record_id'})
 
-new_col = []
-for t in tqdm(df_predictions.predicted_record_id):
-    new_col.append(' '.join([str(x) for x in t]))
+#new_col = []
+#for t in tqdm(df_predictions.predicted_record_id):
+#    new_col.append(' '.join([str(x) for x in t]))
 
-df_predictions.predicted_record_id = new_col
-df_predictions.to_csv('xgb_sub8.csv', index=False)
+#df_predictions.predicted_record_id = new_col
+#df_predictions.to_csv('xgb_sub8.csv', index=False)
